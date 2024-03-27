3b16d13395451da5a36572edc22d1c96086484fc
tree <hhyjs2r@gmail.com>
2024-03-27 21:20:20 +0900

    feat: create board modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [],
+  imports: [BoardsModule],
   controllers: [],
   providers: [],
 })
