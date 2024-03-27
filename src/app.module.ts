7cede5540d715c1d464a5d646c629d3eda9e04f5
tree <hhyjs2r@gmail.com>
2024-03-27 22:50:20 +0900

    feat: injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
+import { BoardsController } from './boards/boards.controller';
+import { BoardsService } from './boards/boards.service';
 
 @Module({
   imports: [BoardsModule],
-  controllers: [],
-  providers: [],
+  controllers: [BoardsController],
+  providers: [BoardsService],
 })
 export class AppModule {}
