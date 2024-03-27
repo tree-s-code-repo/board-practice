5b05e6f90c23df8314609d889820eb73672ab099
tree <hhyjs2r@gmail.com>
2024-03-27 23:14:15 +0900

    feat: add injection to controller

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardService: BoardsService) {}
+
+  @Get()
+  getAllBoard() {
+    return this.boardService.getAllBoards();
+  }
 }
