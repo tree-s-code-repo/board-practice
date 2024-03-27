4bd383068046cd8279ece8cf72d63e9996ae141f
tree <hhyjs2r@gmail.com>
2024-03-28 02:55:41 +0900

    config: add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { BoardsController } from './boards/boards.controller';
 import { BoardsService } from './boards/boards.service';
+import { typeORMConfig } from './boards/configs/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
---
+++ src/boards/configs/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: 5432,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DATABASE,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+};
