8129c5cb1ebece2c91204a889e257ca7ad2b7b85
tree <hhyjs2r@gmail.com>
2024-04-01 00:18:33 +0900

    feat: create acessToken logic

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,32 @@ import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  exports: [AuthService],
+  imports: [
+    ConfigModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: process.env.JWT_SECRET || 'your_default_secret',
+        signOptions: { expiresIn: 3600 },
+      }),
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserRepository],
 })
 export class AuthModule {}
+
+// PassportModule.register({ defaultStrategy: 'jwt' }),
+// JwtModule.register({
+//   secret: process.env.JWT_SECRET,
+//   signOptions: {
+//     expiresIn: 3600,
+//   }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async getAll() {
@@ -19,11 +21,17 @@ export class AuthService {
     return this.userRepository.createUser(authCredentialDto);
   }
 
-  async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+  async signIn(
+    authCredentialDto: AuthCredentialDto,
+  ): Promise<{ acessToken: string }> {
     const { username, password } = authCredentialDto;
     const user = await this.userRepository.findOneBy({ username });
+
     if (user && (await compare(password, user.password))) {
-      return 'login sucess';
+      const payload = { username };
+      const acessToken = await this.jwtService.sign(payload);
+
+      return { acessToken };
     }
 
     throw new Error('login failed');
